a167c65b046d65766a17475b8823eedfa1b54608
KevinWang <kevinwang2020@sina.com>
2023-04-15 16:31:06 +0800

    update config

--- src/server/prod-config.js
+++ src/server/prod-config.js
@@ -4,7 +4,7 @@ const config = {
     BASE_URL: 'http://8.134.105.120:8085',
     PORT: 8028,
     SECURE_COOKIE: false,
-    URI: '127.0.0.1:80',
+    URI: '127.0.0.1:8028',
     AUTH_COOKIE_PASSWORD: 'KEVP3DazkI31GfilXjtSp0ZvJy00taKf',
     EXCHANGE_RATE: 6.6
 };
