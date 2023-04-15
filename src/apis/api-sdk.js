d4fd41ca4aabcc1f438f35c38b31e316fbe6bc26
KevinWang <kevinwang2020@sina.com>
2023-04-15 16:45:36 +0800

    update

--- src/apis/api-sdk.js
+++ src/apis/api-sdk.js
@@ -1,7 +1,7 @@
 const ApiHelper = require('../common/ApiHelper');
-const util = require('../common/env-util');
+// const util = require('../common/env-util');
 
-const BaseURL = util.readEnvValue('BASE_URL', null);
+// const BaseURL = util.readEnvValue('BASE_URL', null);
 
 class ApiSdk {
     constructor(contextId) {
@@ -10,15 +10,15 @@ class ApiSdk {
 
     /** User */
     async userSignup(data) {
-        return ApiHelper.post(`${BaseURL}/user/register`, data, this.contextId);
+        return ApiHelper.post(`123/user/register`, data, this.contextId);
     }
 
     async userPasswordReset(data) {
-        return ApiHelper.post(`${BaseURL}/user/updatePassword`, data, this.contextId);
+        return ApiHelper.post(`123/user/updatePassword`, data, this.contextId);
     }
 
     async getAuthCode(phone) {
-        return ApiHelper.get(`${BaseURL}/user/getAuthCode?phone=${phone}`, this.contextId);
+        return ApiHelper.get(`123/user/getAuthCode?phone=${phone}`, this.contextId);
     }
 }
 
