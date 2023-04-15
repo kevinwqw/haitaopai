90fec91937a92f35f57ef58d5f4c3423dc4ed8d3
KevinWang <kevinwang2020@sina.com>
2023-04-15 16:18:35 +0800

    update port

--- src/server/prod-config.js
+++ src/server/prod-config.js
@@ -2,7 +2,7 @@ const config = {
     DEBUG: false,
     NODE_ENV: 'production',
     BASE_URL: 'http://8.134.105.120:8085',
-    PORT: 80,
+    PORT: 8028,
     SECURE_COOKIE: false,
     URI: '127.0.0.1:80',
     AUTH_COOKIE_PASSWORD: 'KEVP3DazkI31GfilXjtSp0ZvJy00taKf',
