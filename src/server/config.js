7774ce157a878106f01e8db653c8a6bd30730c19
KevinWang <kevinwang2020@sina.com>
2023-04-15 17:09:08 +0800

    update

--- src/server/config.js
+++ src/server/config.js
@@ -3,6 +3,6 @@ const util = require('../common/env-util');
 module.exports = {
     password: util.readEnvValue('AUTH_COOKIE_PASSWORD', 'KEVP6D9zkI31GNilVjtSp0rvJy00taKf'),
     isSecure: util.readBoolEnvValue('SECURE_COOKIE', false),
-    uri: util.readEnvValue('URI', '127.0.0.1:80')
-    // baseUrl: util.readEnvValue('BASE_URL')
+    uri: util.readEnvValue('URI', '127.0.0.1:80'),
+    baseUrl: util.readEnvValue('BASE_URL')
 };
