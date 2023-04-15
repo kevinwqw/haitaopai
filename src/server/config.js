aeabef8ac29b6c209d7a42d1c03143450a614d68
KevinWang <kevinwang2020@sina.com>
2023-04-15 16:26:24 +0800

    add baseUrl

--- src/server/config.js
+++ src/server/config.js
@@ -4,5 +4,6 @@ module.exports = {
     password: util.readEnvValue('AUTH_COOKIE_PASSWORD', 'KEVP6D9zkI31GNilVjtSp0rvJy00taKf'),
     isSecure: util.readBoolEnvValue('SECURE_COOKIE', false),
     uri: util.readEnvValue('URI', '127.0.0.1:80'),
+    baseUrl: util.readEnvValue('BASE_URL'),
     baseUrl: util.readEnvValue('BASE_URL')
 };
