691c9b1f721d171c2d357462d7665731aaa5fa69
KevinWang <kevinwang2020@sina.com>
2023-04-15 15:43:47 +0800

    update Port

--- src/server/dev.js
+++ src/server/dev.js
@@ -2,9 +2,9 @@ const generalConfig = {
     DEBUG: true,
     NODE_ENV: 'development',
     BASE_URL: 'http://8.134.105.120:8085',
-    PORT: 8080,
+    PORT: 8028,
     SECURE_COOKIE: false,
-    URI: '127.0.0.1:8080',
+    URI: '127.0.0.1:8028',
     AUTH_COOKIE_PASSWORD: 'KEVP6D9zkI31GNilVjtSp0rvJy00taKf'
 };
 
